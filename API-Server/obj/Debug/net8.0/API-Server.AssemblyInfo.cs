b38335aa4962e0b43c1d89ea1ab6cd59419dce59
Lil-Trill <danil.yurin.1990@mail.ru>
2024-04-17 22:28:25 +0300

    next commit

--- API-Server/obj/Debug/net8.0/API-Server.AssemblyInfo.cs
+++ API-Server/obj/Debug/net8.0/API-Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9764554ad69fd8fdd115a8dfed79869a8fdc80a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
