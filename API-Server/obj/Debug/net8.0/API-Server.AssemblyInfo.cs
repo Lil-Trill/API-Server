b909f3dfb42c898341d8203325e419082ec3df90
Lil-Trill <danil.yurin.1990@mail.ru>
2024-04-19 17:38:58 +0300

    last

--- API-Server/obj/Debug/net8.0/API-Server.AssemblyInfo.cs
+++ API-Server/obj/Debug/net8.0/API-Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dd38d0c30edb3e5d07301de8e29180ec57aaed6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd734371c526444b31e7044c07968013104e98c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
